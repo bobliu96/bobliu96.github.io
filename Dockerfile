ea09332c3fa45d2907e6728eb604ef870b09034d
Bo Liu <lb1996914@gmail.com>
2022-07-19 01:44:19 -0400

    Add deployment configurations, Update resume

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# pull official base image
+FROM node:13.12.0-alpine
+
+# set working directory
+WORKDIR /app
+
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+ENV CHOKIDAR_USEPOLLING: "true"
+
+# install app dependencies
+COPY ./package.json ./
+RUN npm install
+RUN npm install react-scripts@3.4.1 -g --silent
+
+# add app
+COPY . ./
+
+# start app
+CMD ["npm", "start"]
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 Xuanlin Wang</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2022 Bo Liu</li>
+              <li>Design by <a title="Styleshout" href="https://boliu.ca/">Bo Liu</a></li>
            </ul>
 
         </div>
